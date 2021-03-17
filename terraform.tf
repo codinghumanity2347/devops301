8bfa8ef93841ac4287aa41e53e2f49c7a86ad135
shreya panseja <shreyapnseja@gmail.com>
2021-03-17 12:13:41 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,3 +6,18 @@ resource "azurerm_resource_group" "k8s" {
   name     = "gitops-team1"
   location = "East US 2"
 }
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "automated-cluster"
+  location            = "East US 2"
+  resource_group_name = "gitops"
+
+  
+
+  agent_pool_profile {
+    name            = "default"
+    count           = 1
+    vm_size         = "Standard_DS2_v2"
+    os_type         = "Linux"
+    os_disk_size_gb = 7
+  }
